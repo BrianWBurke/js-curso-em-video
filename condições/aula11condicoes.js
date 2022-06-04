d43d82d9edddd2f24ac6b53d6bd1bab28525911c
Brian <brianetec@gmail.com>
2022-06-04 10:38:31 -0300

    commit condições ex 01 e arquivo limpo ex 02

---
+++ condições/aula11condicoes.js
@@ -0,0 +1,9 @@
+var vel = 59
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velociade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
