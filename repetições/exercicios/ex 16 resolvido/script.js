22536171e0b462b9ee88b9c5b8c000ac8ae1fc7c
Brian <brianetec@gmail.com>
2022-06-07 15:56:24 -0300

    ex 16 corrigido

--- repetições/exercicios/ex 16 resolvido/script.js
+++ repetições/exercicios/ex 16 resolvido/script.js
@@ -5,18 +5,30 @@ function contar() {
     let passo = document.getElementById('txtp')
     let res = document.querySelector('div#res')
 
-if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
-window.alert('[ERRO] Faltam dados!')
-} else {
-    res.innerHTML = 'Contando: '
-    let i = Number(ini.value)
-    let f = Number(fim.value)
-    let p = Number(passo.value)
-
-    for(let c = i; c <= f; c += p) {
-        res.innetHTML += ` ${c} \u{27A1}`
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+       // window.alert('[ERRO] Faltam dados!')        
+    } else {
+        res.innerHTML = `Contando: \u{25B6} \u{27A1}`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            //caso passo for <= 0
+            window.alert('Passo inválido! Considernado PASSO: 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{27A1}`
+            }
+        } else {
+            //contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{27A1}`
+            }            
+        }
+        res.innerHTML += `\u{23F9}`
     }
-
-}
-
 }
